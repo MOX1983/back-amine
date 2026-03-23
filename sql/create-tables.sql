1c31808dccee226c72c98d466e795f96fba65c0e
Mox <tenmaken246@gmail.com>
2026-03-23 18:40:12 +0300

    sql

---
+++ sql/create-tables.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE "anime"(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL,
+	"time_stop" TIME,
+	"number_episode" INT
+);
+
+CREATE TABLE "users" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL UNIQUE,
+	"email" VARCHAR(100) NOT NULL UNIQUE,
+	"password" VARCHAR(50) NOT NULL,
+	"id_anime" INT,
+
+	FOREIGN KEY ("id_anime") REFERENCES "anime" ("id")
+);
+
+CREATE TABLE "user_anime"(
+	"user_id" INT,
+	"anime_id" INT
+);
+
